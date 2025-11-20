29b34c38a066ede044c251f5c9364055a9bb8089
Lintang <lintangwaladi@gmail.com>
2025-11-20 13:51:09 +0700

    random tips

--- lib/controllers/dashboard_controller.dart
+++ lib/controllers/dashboard_controller.dart
@@ -3,6 +3,7 @@ import '../models/weather_model.dart';
 import '../models/farm_model.dart';
 import '../services/dummy_weather_service.dart';
 import '../services/firestore_service.dart';
+import 'dart:math';
 
 class DashboardController extends GetxController {
   final DummyWeatherService _weatherService = DummyWeatherService();
@@ -15,12 +16,14 @@ class DashboardController extends GetxController {
   
   // Stream list lahan (otomatis update jika ada perubahan di Firestore)
   Stream<List<FarmModel>> get farmListStream => _firestoreService.getFarms();
+  var dailyTips = <Map<String, String>>[].obs;
 
   @override
   void onInit() {
     super.onInit();
     updateGreeting();
     fetchWeather();
+    ever(currentWeather, (_) => generateSmartTips());
   }
 
   // 1. Logika Sapaan (Pagi/Siang/Sore)
@@ -46,5 +49,45 @@ class DashboardController extends GetxController {
     } finally {
       isLoadingWeather.value = false;
     }
+  }
+
+  void generateSmartTips() {
+    if (currentWeather.value == null) return;
+
+    var w = currentWeather.value!;
+    var tips = <Map<String, String>>[];
+
+    // 1. Logika Berdasarkan Cuaca
+    if (w.temperature > 30) {
+      tips.add({
+        "title": "Cuaca Panas Terik",
+        "body": "Hindari penyemprotan pestisida siang hari agar tidak menguap. Siram lahan saat sore.",
+        "type": "warning" // untuk warna icon
+      });
+    } else if (w.condition.toLowerCase().contains("hujan")) {
+      tips.add({
+        "title": "Potensi Hujan Turun",
+        "body": "Tunda pemupukan tabur agar tidak hanyut terbawa air. Cek saluran irigasi.",
+        "type": "info"
+      });
+    }
+
+    // 2. Logika Berdasarkan Musim
+    if (w.season == "Musim Kemarau") {
+      tips.add({
+        "title": "Waspada Kutu Daun",
+        "body": "Cuaca kering memicu ledakan populasi Kutu. Pasang perangkap kuning sekarang.",
+        "type": "alert"
+      });
+    }
+
+    // 3. Logika Umum (Randomizer biar tidak kosong)
+    var generalTips = [
+      {"title": "Info Pasar", "body": "Harga cabai rawit stabil di Rp 45.000/kg hari ini.", "type": "success"},
+      {"title": "Tips Hemat", "body": "Gunakan air cucian beras untuk pupuk cair tambahan.", "type": "info"},
+    ];
+    tips.add(generalTips[Random().nextInt(generalTips.length)]);
+
+    dailyTips.assignAll(tips);
   }
 }
